b22af4645e363e8b5b676fb11103b4e41be66c96
alliedeaver <adeav97@gmail.com>
2023-09-14 22:36:16 -0700

    changes to index

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const myJSON = JSON.stringify(inquirer);
-
-
 
 //// TODO: Create an array of questions for user input
-
-
 
 const questions = [
     {
@@ -31,7 +26,6 @@ const questions = [
     },
 
 
-
     {
         type: 'input',
         message: 'Usage Information',
@@ -45,17 +39,20 @@ const questions = [
         name: 'Contribution',
     },
 
+
     {
         type: 'input',
         message: 'Test instructions',
         name: 'tests',
     },
 
+
     {
         type: 'input',
         message: 'Which license did you use?',
         name: 'license',
     },
+
 
     {
         type: 'input',
@@ -66,61 +63,36 @@ const questions = [
 ];
 
 
-// const writeToFile = (answers) => {
-   
-//     console.log(answers)
-//     fs.writeFile('log.txt', process.argv[2], (err) =>
-//         err ? console.error(err) : console.log('Success!')
-//     );
-// }
-
-
-
 // TODO: Create a function to write README file
 
-// const README = 
-// function writeToFile(README, data) { }
 
+const writeToFile = (filename, data) => {
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) => {
+        if(err) {
+            return console.error(err);
+        }
+        console.log('Written to File!');
+    })
+};
 
+    
 
 // TODO: Create a function to initialize app
 // function init() { }
 
 const init = () => {
     inquirer.prompt(questions)
-        .then((response) => console.log(response));
-}
+        .then((response) => {
+            writeToFile('output.json', response);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
 
+
+init (); 
 
 
 // Function call to initialize app
-// const writeToFile = (answers) => {
-//     console.log(answers)
-//     fs.writeFile('log.txt', process.argv[2], (err) =>
-//   err ? console.error(err) : console.log('Success!')
-// );
-// } 
-    
-// init(questions, writeToFile);
 
-
-
-
-// fs.writeFile('log.txt', process.argv[2], (err) =>
-//   err ? console.error(err) : console.log('Success!')
-// );
-
-
-// const responseHandler = (response) => (
-//     response.confirm === response.password
-//       ? console.log('Success!')
-//       : console.log('')
-//   );
-  
- 
-  
-
-
-
-//   fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>
-//   )
